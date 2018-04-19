f47f2974b3bd1c6c584d61752404721ad535f512
Wilson choi <wilsonchoi@Wilsons-MacBook-Pro.local>
2018-04-19 09:38:08 +0800

    add lab

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/api/workshops', function (req, res) {
 app.get('/api/workshops/roadshow', function (req, res) {
    res.setHeader('Content-Type', 'application/json');
    //res.send(JSON.stringify({"id":"roadshow","modules":["environment","install","explore","docker","scaling","routes","logging","permissions",remote-ops","java","databases","configmap","application-health","pipelines","codechanges","templates","clustering","further-resources"],"name":"Roadshow"}));
-   res.send(JSON.stringify({"id":"roadshow","modules":["environment","explore","docker","scaling","routes","logging","permissions","java","databases", "further-resources" ],"name":"Roadshow"
+   res.send(JSON.stringify({"id":"roadshow","modules":["environment","explore","docker","scaling","routes","logging","permissions","java","databases","application-health", "further-resources" ],"name":"Roadshow"
 }));
 })
 
